298c2b65553b3d1945ca4ed1f677df86e3fbf5fe
Henry Bulmer <hbulmer@callutheran.edu>
2017-04-20 16:42:16 -0700

    POSLibrary reads and writes to/from file
    If a POSLibrary has already been generated it will use that instead of
    making one (drasitacally increasing performance). Once it makes a
    library it writes it to a file. These values are space separated.

--- src/Capstone/POSLibrary.java
+++ src/Capstone/POSLibrary.java
@@ -1,6 +1,12 @@
 package Capstone;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,13 +18,50 @@ public class POSLibrary extends Tagger{
 	HashMap<String, ArrayList<String>> lib;
 	HashMap<String, ArrayList<String>> accessable;
 	
-	public POSLibrary (String fileName) throws FileNotFoundException{
-		lib = new HashMap<String, ArrayList<String>>();
-		
-		List<List<TaggedWord>> tagged = tagFile(fileName);
-		populateLib(tagged);
+	String LIB_TXT = "lib.txt"; 
+	
+	public POSLibrary (String fileName) throws IOException{
+		initializeLib(fileName);
 		
 		System.err.println("Terminated");
+	}
+	
+	private void initializeLib (String fileName) throws IOException{
+		lib = new HashMap<String, ArrayList<String>>();
+		
+		//if it is initialized it will grab the data from a file
+		//otherwise it will create the lib and write it to a file
+		if (!isInitialized()){
+			List<List<TaggedWord>> tagged = tagFile(fileName);
+			populateLib(tagged);
+			writeLibToFile();
+		}
+	}
+	
+	private boolean isInitialized() throws IOException{
+		BufferedReader reader = null;
+		try{
+			File file = new File(LIB_TXT);
+			reader = new BufferedReader(new FileReader(file)); 
+			
+			String line;
+			while ((line = reader.readLine()) != null){
+				String[] words = line.split(" ");
+				lib.put(words[0], new ArrayList<String>());
+				
+				for (int i = 1; i < words.length; i++){
+					lib.get(words[0]).add(words[i]);
+				}
+				
+//				System.out.println(line);
+			}
+			
+			reader.close();
+			return true;
+			
+		} catch (FileNotFoundException e){
+			return false;
+		}
 	}
 	
 	private void populateLib (List<List<TaggedWord>> tagged){
@@ -31,6 +74,24 @@ public class POSLibrary extends Tagger{
 				 }
 			 }
 		 }
+	}
+	
+	private void writeLibToFile(){
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(LIB_TXT))) {
+
+		    for (Entry<String, ArrayList<String>> entry : lib.entrySet()) {
+		        bw.write(entry.getKey()+" ");
+		        for(String word : entry.getValue()){
+		            bw.write(word+" ");
+		        }
+		        bw.newLine();
+		    }
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
 	}
 	
 	public void displayData(){
